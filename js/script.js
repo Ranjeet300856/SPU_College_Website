80d20294c119f0a945fb11afe24b12eb21f33392
Ranjeet Suthar <ranjeetsuthar194@gmail.com>
2026-05-30 09:58:22 +0530

    Bug-Fix

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ function toggleMenu() {
     const navBar = document.getElementById("navLinks");
     navBar.classList.toggle("active");
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("#navLinks a").forEach(link => {
+        link.addEventListener("click", () => {
+            document.getElementById("navLinks").classList.remove("active");
+        });
+    });
+});
